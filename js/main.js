6eacff94f28dfef427748b6eac6822552d35d6b0
FxMajid <fadlinmajid459@gmail.com>
2020-07-22 10:20:19 +0700

    count pairs

--- js/main.js
+++ js/main.js
@@ -86,4 +86,3 @@ function editbtn(event){
         price.value     = data.price|| "";
     })
 }
-
---
+++ src/solution.js
@@ -0,0 +1,14 @@
+let x = (array, sum) => {
+    let hashMap = {},
+      results = []
+
+        for (let i = 0; i < array.length; i++){
+            if (hashMap[array[i]]){
+                results.push([hashMap[array[i]], array[i]])
+            }else{
+                hashMap[sum - array[i]] = array[i];
+            }
+          }
+          return results;
+    }
+console.log(x([2, 3, 6 ,7, 9, 11],9));
